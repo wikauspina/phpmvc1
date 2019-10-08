8e51e6ecd3b43bbcd3b643fd85734c40478f28b3
wikauspina <wikauspina@gmail.com>
2019-10-08 09:50:15 +0700

    menambahkan file About.php

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 
+ */
+class About {
+	public function index($nama = 'wika', $pekerjaan = 'mahasiswa')
+	{
+		echo "hallo nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+	public function page ()
+	{
+		echo 'About/page';
+	}
+}
