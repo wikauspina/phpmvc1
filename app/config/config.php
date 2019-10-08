18ceb1e6fd110ed183019aea2385c9f2b9d7c22d
wikauspina <wikauspina@gmail.com>
2019-10-08 21:27:53 +0700

    menambahkan file detail.php

--- app/config/config.php
+++ app/config/config.php
@@ -7,5 +7,5 @@ define('BASEURL', 'http://localhost/phpmvc1/public');
 //DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'phpmvc1');
