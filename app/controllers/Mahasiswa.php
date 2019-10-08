12ab8b41d2913e7383587d3d2d5c3adba3fe96f9
wikauspina <wikauspina@gmail.com>
2019-10-08 19:59:47 +0700

    menambahkan file Mahasiswa_model.php

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -7,8 +7,9 @@ class Mahasiswa extends Controller{
 	public function index()
 	{
 		$data['judul'] = 'Daftar Mahasiswa';
+		$data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
 		$this->view('templates/header', $data);
-		$this->view('mahasiswa/index');
+		$this->view('mahasiswa/index', $data);
 		$this->view('templates/footer');	
 	}
 }
---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,23 @@
+<?php
+
+class Mahasiswa_model{
+	private $dbh;
+	private $stmt;
+
+	public function __construct()
+	{
+		$dsn = 'mysql:host=localhost;dbname=phpmvc1';
+
+		try{
+			$this->dbh = new PDO($dsn, 'root', '');
+		}catch(PDOException $e){
+			die($e->getMessage());
+		}
+	}
+
+	public function getAllMahasiswa(){
+		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+		$this->stmt->execute();
+		return $this->stmt->fetchAll();
+	}
+}
